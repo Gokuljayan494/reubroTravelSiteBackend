3561983997d6aa9de88dc457191245ebeae9c50e
Gokuljayan494 <gokuljayan494@gmail.com>
2023-01-23 12:34:12 +0530

    new 18 commit

--- controllers/mystiflyController.js
+++ controllers/mystiflyController.js
@@ -11,7 +11,7 @@ exports.mystiflyApiSearch = async (req, res) => {
       url: "https://restapidemo.myfarebox.com/api/v1/Search/Flight",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer 18AEA8F0-5B21-41ED-9993-DD7A8123B0D2-1560",
+        Authorization: `Bearer ${process.env.MYSTIFLY_TOKEN} `,
       },
       data: {
         OriginDestinationInformations: [
