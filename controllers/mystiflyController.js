a940fc8f8407d706e7c3fb381ed42c8c44211d35
Gokuljayan494 <gokuljayan494@gmail.com>
2023-01-24 12:02:33 +0530

    new 19 commit

--- controllers/mystiflyController.js
+++ controllers/mystiflyController.js
@@ -1,65 +1,255 @@
 const axios = require("axios");
+let response;
+let ADT;
+let CHD;
+const oneWay = async function (
+  DepartureDateTime,
+  OriginLocationCode,
+  DestinationLocationCode,
+  AirType,
+  ADT,
+  CHD,
+  INF
+) {
+  console.log(`-----------------`);
+  console.log(ADT, CHD, INF);
+  const response = await axios({
+    method: "post",
+    url: "https://restapidemo.myfarebox.com/api/v2/Search/Flight",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.MYSTIFLY_TOKEN} `,
+    },
+    data: {
+      OriginDestinationInformations: [
+        {
+          DepartureDateTime: `${DepartureDateTime}T00:00:00`,
+          OriginLocationCode: OriginLocationCode,
+          DestinationLocationCode: DestinationLocationCode,
+        },
+      ],
+      TravelPreferences: {
+        MaxStopsQuantity: "Direct",
+        VendorPreferenceCodes: ["EK"],
+        CabinPreference: "Y",
+        Preferences: {
+          CabinClassPreference: {
+            CabinType: "Y",
+            PreferenceLevel: "Restricted",
+          },
+        },
+        AirTripType: "OneWay",
+      },
+      PricingSourceType: "Public",
+      IsRefundable: true,
+      PassengerTypeQuantities: [
+        {
+          Code: "ADT",
+          Quantity: ADT,
+        },
+        {
+          Code: "CHD",
+          Quantity: CHD,
+        },
+      ],
 
+      RequestOptions: "Fifty",
+      NearByAirports: true,
+      Target: "Test",
+      ConversationId: "string",
+    },
+  });
+
+  return response;
+};
+const returnTwoWay = async function (
+  DepartureDateTime,
+  OriginLocationCode,
+  DestinationLocationCode,
+  AirType,
+  ADT,
+  CHD,
+  INF
+) {
+  console.log(ADT);
+  const response = await axios({
+    method: "post",
+    url: "https://restapidemo.myfarebox.com/api/v2/Search/Flight",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.MYSTIFLY_TOKEN} `,
+    },
+    data: {
+      OriginDestinationInformations: [
+        {
+          DepartureDateTime: `${DepartureDateTime}T00:00:00`,
+          OriginLocationCode: OriginLocationCode,
+          DestinationLocationCode: DestinationLocationCode,
+        },
+        {
+          DepartureDateTime: "2023-02-27T00:00:00",
+          OriginLocationCode: "BLR",
+          DestinationLocationCode: "DXB",
+        },
+      ],
+      TravelPreferences: {
+        MaxStopsQuantity: "Direct",
+        VendorPreferenceCodes: ["EK"],
+        CabinPreference: "Y",
+        Preferences: {
+          CabinClassPreference: {
+            CabinType: "Y",
+            PreferenceLevel: "Restricted",
+          },
+        },
+        AirTripType: "Return",
+      },
+      PricingSourceType: "Public",
+      IsRefundable: true,
+      PassengerTypeQuantities: [
+        {
+          Code: "ADT",
+          Quantity: ADT,
+        },
+        {
+          Code: "CHD",
+          Quantity: CHD,
+        },
+      ],
+      RequestOptions: "Fifty",
+      NearByAirports: true,
+      Target: "Test",
+      ConversationId: "string",
+    },
+  });
+  return response;
+};
+
+const roundFlight = async function (
+  DepartureDateTime,
+  OriginLocationCode,
+  DestinationLocationCode,
+  AirType,
+  ADT,
+  CHD,
+  INF
+) {
+  const response = await axios({
+    method: "post",
+    url: "https://restapidemo.myfarebox.com/api/v1/Search/Flight",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.MYSTIFLY_TOKEN} `,
+    },
+    data: {
+      OriginDestinationInformations: [
+        {
+          DepartureDateTime: `${DepartureDateTime}T00:00:00`,
+          OriginLocationCode: OriginLocationCode,
+          DestinationLocationCode: DestinationLocationCode,
+        },
+        {
+          DepartureDateTime: "2023-02-25T00:00:00",
+          OriginLocationCode: "DXB",
+          DestinationLocationCode: "BLR",
+        },
+      ],
+      TravelPreferences: {
+        MaxStopsQuantity: "Direct",
+        VendorPreferenceCodes: ["EK"],
+        CabinPreference: "Y",
+        Preferences: {
+          CabinClassPreference: {
+            CabinType: "Y",
+            PreferenceLevel: "Restricted",
+          },
+        },
+        AirTripType: "Circle",
+      },
+      PricingSourceType: "Public",
+      IsRefundable: true,
+      PassengerTypeQuantities: [
+        {
+          Code: "ADT",
+          Quantity: ADT,
+        },
+        {
+          Code: "CHD",
+          Quantity: CHD,
+        },
+        {
+          Code: "INF",
+          Quantity: INF,
+        },
+      ],
+      RequestOptions: "Fifty",
+      NearByAirports: true,
+      Target: "Test",
+      ConversationId: "string",
+    },
+  });
+  return response;
+};
 exports.mystiflyApiSearch = async (req, res) => {
   try {
-    const { DepartureDateTime, OriginLocationCode, DestinationLocationCode } =
-      req.body;
-    console.log(req.body);
-    if (!req.body) {
-      throw new Error("Enter fields properly");
+    let {
+      DepartureDateTime,
+      OriginLocationCode,
+      DestinationLocationCode,
+      AirType,
+      ADT,
+      CHD,
+      INF,
+    } = req.body;
+    if (ADT === undefined) {
+      ADT = 1;
     }
-    const response = await axios({
-      method: "post",
-      url: "https://restapidemo.myfarebox.com/api/v1/Search/Flight",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.MYSTIFLY_TOKEN} `,
-      },
-      data: {
-        OriginDestinationInformations: [
-          {
-            DepartureDateTime: `${DepartureDateTime}T00:00:00`,
-            OriginLocationCode: OriginLocationCode,
-            DestinationLocationCode: DestinationLocationCode,
-          },
-          //   {
-          //     DepartureDateTime: "2023-02-25T00:00:00",
-          //     OriginLocationCode: "DXB",
-          //     DestinationLocationCode: "BLR",
-          //   },
-        ],
-        TravelPreferences: {
-          MaxStopsQuantity: "Direct",
-          VendorPreferenceCodes: ["EK"],
-          CabinPreference: "Y",
-          Preferences: {
-            CabinClassPreference: {
-              CabinType: "Y",
-              PreferenceLevel: "Restricted",
-            },
-          },
-          AirTripType: "OneWay",
-        },
-        PricingSourceType: "Public",
-        IsRefundable: true,
-        PassengerTypeQuantities: [
-          {
-            Code: "ADT",
-            Quantity: 1,
-          },
-        ],
-        RequestOptions: "Fifty",
-        NearByAirports: true,
-        Target: "Test",
-        ConversationId: "string",
-      },
-    });
+    if (CHD === undefined) {
+      CHD = 0;
+    }
+    if (INF === undefined) {
+      INF = 0;
+    }
+    if (AirType === "OneWay") {
+      response = oneWay(
+        DepartureDateTime,
+        OriginLocationCode,
+        DestinationLocationCode,
+        AirType,
+        ADT,
+        CHD,
+        INF
+      );
+    }
+    if (AirType === "return") {
+      response = returnTwoWay(
+        DepartureDateTime,
+        OriginLocationCode,
+        DestinationLocationCode,
+        AirType,
+        ADT,
+        CHD,
+        INF
+      );
+    }
+    if (AirType === "Round") {
+      response = roundFlight(
+        DepartureDateTime,
+        OriginLocationCode,
+        DestinationLocationCode,
+        AirType,
+        ADT,
+        CHD,
+        INF
+      );
+    }
 
-    if (!response) {
-      throw new error("no flights");
-    }
-    data = response.data.Data.PricedItineraries;
-    console.log(response.data.Data.PricedItineraries);
+    console.log(req.body);
+
+    data = await response;
+    console.log(data);
+    data = data.data.Data.PricedItineraries;
+    // console.log(response.data.Data.PricedItineraries);
     res.status(200).json({ status: "sucess", data });
   } catch (err) {
     res.status(400).json({ status: "fail", message: `Error:${err.message}` });
