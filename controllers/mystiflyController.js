1bc76f6b0bcccfee363d9ad37a276c4aa437f58c
Gokuljayan494 <gokuljayan494@gmail.com>
2023-02-01 15:52:21 +0530

    29 commit

--- controllers/mystiflyController.js
+++ controllers/mystiflyController.js
@@ -361,6 +361,7 @@ exports.mystiflyApiSearch = async (req, res) => {
     if (req.body.MaxStopsQuantity === undefined) {
       MaxStopsQuantity = "Direct";
     }
+    response = "";
     if (AirType === "OneWay" && CHD === 0) {
       console.log(AirType);
 
